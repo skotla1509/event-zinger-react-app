6eea4df43b6b5f1d4217c60eba3063d3dc245b96
skotla1509 <sindhukotla1509@gmail.com>
2022-12-15 00:35:50 -0500

    home

--- src/views/navigation.js
+++ src/views/navigation.js
@@ -7,44 +7,46 @@ const Navigation = () => {
     const {pathname} = useLocation()
     const parts = pathname.split('/')
     return(
+      <div className="mt-4">
         <ul className="nav nav-pills">
-            <li className="nav-item">
-                <Link to="/"
-                      className={`nav-link ${parts[1] === ''?'active': ''}`}>
-                    Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/search"
-                      className={`nav-link ${parts[1] === 'search'?'active': ''}`}>
-                    Search
-                </Link>
-            </li>
-            <li className={`nav-item ${!currentUser ? 'd-none':''}`}>
-                <Link to="/users"
-                      className={`nav-link ${parts[1] === 'users'?'active': ''}`}>
-                    Users
-                </Link>
-            </li>
-            <li className={`nav-item ${currentUser ? 'd-none':''}`}>
-                <Link to="/login"
-                      className={`nav-link ${parts[1] === 'login'?'active': ''}`}>
-                    Login
-                </Link>
-            </li>
-            <li className={`nav-item ${currentUser ? 'd-none':''}`}>
-                <Link to="/register"
-                      className={`nav-link ${parts[1] === 'register'?'active': ''}`}>
-                    Register
-                </Link>
-            </li>
-            <li className={`nav-item ${!currentUser ? 'd-none':''}`}>
-                <Link to="/profile"
-                      className={`nav-link ${parts[1] === 'profile'?'active': ''}`}>
-                    Profile
-                </Link>
-            </li>
+          <li className="nav-item">
+            <Link to="/"
+                  className={`nav-link ${parts[1] === ''?'active': ''}`}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/search"
+                  className={`nav-link ${parts[1] === 'search'?'active': ''}`}>
+              Search
+            </Link>
+          </li>
+          <li className={`nav-item ${!currentUser ? 'd-none':''}`}>
+            <Link to="/users"
+                  className={`nav-link ${parts[1] === 'users'?'active': ''}`}>
+              Users
+            </Link>
+          </li>
+          <li className={`nav-item ${currentUser ? 'd-none':''}`}>
+            <Link to="/login"
+                  className={`nav-link ${parts[1] === 'login'?'active': ''}`}>
+              Login
+            </Link>
+          </li>
+          <li className={`nav-item ${currentUser ? 'd-none':''}`}>
+            <Link to="/register"
+                  className={`nav-link ${parts[1] === 'register'?'active': ''}`}>
+              Register
+            </Link>
+          </li>
+          <li className={`nav-item ${!currentUser ? 'd-none':''}`}>
+            <Link to="/profile"
+                  className={`nav-link ${parts[1] === 'profile'?'active': ''}`}>
+              Profile
+            </Link>
+          </li>
         </ul>
+      </div>
     )
 }
 
